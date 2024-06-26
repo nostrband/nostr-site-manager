87481604b07d4756597035a514fd64c99ec6490a
a-fralou <afralou@yandex.by>
2024-06-26 13:33:56 +0300

    Add check website address & add debounce for check

--- src/components/SettingPage/components/URL/index.tsx
+++ src/components/SettingPage/components/URL/index.tsx
@@ -1,34 +1,51 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, ChangeEvent } from "react";
 import {
   StyledFormControl,
   StyledHeadSettingBlock,
   StyledSettingBlock,
   StyledSettingCol,
 } from "@/components/SettingPage/styled";
-import { InputLabel, OutlinedInput, Typography } from "@mui/material";
+import { InputLabel, OutlinedInput, Typography, CircularProgress, InputAdornment } from "@mui/material";
 import { SaveButton } from "@/components/SettingPage/components/SaveButton";
 import { useEditSettingMode } from "@/hooks/useEditSettingMode";
 import { IBaseSetting } from "@/types/setting.types";
 import { HASH_CONFIG } from "@/consts";
+import {debounce} from 'lodash';
 
 interface ITitleDescription extends IBaseSetting {
   url: string;
 }
 
 export const URL = ({
-  url,
-  handleChange,
-  handleBlur,
-  submitForm,
-  isLoading,
-}: ITitleDescription) => {
+                      url,
+                      handleChange,
+                      handleBlur,
+                      submitForm,
+                      isLoading,
+                    }: ITitleDescription) => {
   const [isEdit, handleAction] = useEditSettingMode(submitForm);
   const inputRef = useRef<HTMLInputElement>(null);
   const [isDisabled, setDisabled] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [isFetchAddress, setFetchAddress] = useState(false);
+
   const handleClick = () => {
     handleAction().then();
     setDisabled((prev) => !prev);
   };
+
+  const checkUrlExists = debounce((url: string) => {
+    setError(null);
+    setFetchAddress(true)
+    setTimeout(() => {
+      if (true) { // Replace with actual condition or mock
+        setError("This URL already exists.");
+      } else {
+        setError(null);
+      }
+      setFetchAddress(false)
+    }, 2000);
+  }, 300);
 
   useEffect(() => {
     if (inputRef.current && isDisabled) {
@@ -36,36 +53,51 @@ export const URL = ({
     }
   }, [isDisabled]);
 
+  const handleChangeWithDebounce = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    if (handleChange) {
+      handleChange(event);
+    }
+
+    checkUrlExists(value);
+  };
+
   return (
-    <StyledSettingCol id={HASH_CONFIG.URL}>
-      <StyledSettingBlock>
-        <StyledHeadSettingBlock>
-          <Typography variant="h6">Website address</Typography>
+      <StyledSettingCol id={HASH_CONFIG.URL}>
+        <StyledSettingBlock>
+          <StyledHeadSettingBlock>
+            <Typography variant="h6">Website address</Typography>
 
-          <SaveButton
-            isEdit={isEdit}
-            isLoading={isLoading}
-            handleAction={handleClick}
-          />
-        </StyledHeadSettingBlock>
+            <SaveButton
+                isEdit={isEdit}
+                isLoading={isLoading}
+                handleAction={handleClick}
+            />
+          </StyledHeadSettingBlock>
 
-        {/* <Typography variant="body2" sx={{ mb: 1 }}>
-          Website address 
-        </Typography> */}
-
-        <StyledFormControl disabled={!isEdit} fullWidth size="small">
-          <InputLabel htmlFor="url">URL</InputLabel>
-          <OutlinedInput
-            inputRef={inputRef}
-            id="url"
-            name="url"
-            label="URL"
-            onChange={handleChange}
-            value={url}
-            onBlur={handleBlur}
-          />
-        </StyledFormControl>
-      </StyledSettingBlock>
-    </StyledSettingCol>
+          <StyledFormControl disabled={!isEdit} fullWidth size="small">
+            <InputLabel htmlFor="url">URL</InputLabel>
+            <OutlinedInput
+                inputRef={inputRef}
+                id="url"
+                name="url"
+                label="URL"
+                onChange={handleChangeWithDebounce}
+                value={url}
+                onBlur={handleBlur}
+                error={!!error}
+                endAdornment={
+                  isFetchAddress ? (
+                      <InputAdornment position="end">
+                        <CircularProgress size={20} />
+                      </InputAdornment>
+                  ) : null
+                }
+            />
+            {error && <Typography color="error">{error}</Typography>}
+          </StyledFormControl>
+        </StyledSettingBlock>
+      </StyledSettingCol>
   );
 };

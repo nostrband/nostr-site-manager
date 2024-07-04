2d6aacc5f988f1c8f6f8d903a07aba511cece941
a-fralou <afralou@yandex.by>
2024-07-04 16:01:26 +0300

    Fix preview

--- src/components/Pages/Preview/styled.tsx
+++ src/components/Pages/Preview/styled.tsx
@@ -17,3 +17,16 @@ export const StyledPreviewTestSite = styled(Box)(({ theme }) => ({
   overflow: "hidden",
   boxShadow: "0px 28px 40.1px 0px #00000026",
 }));
+
+export const StyledPreviewTestSiteNotAuth = styled(Box)(() => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  height: "100%",
+  paddingBottom: "74px",
+  paddingTop: "64px",
+  position: "relative",
+  overflow: "hidden",
+  boxShadow: "0px 28px 40.1px 0px #00000026",
+}));

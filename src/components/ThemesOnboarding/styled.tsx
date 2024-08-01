3cfb5f057323b5fe18985591f735d69c7d389a4f
artur <brugeman.artur@gmail.com>
2024-08-01 08:44:11 +0200

    Change homepage, add reviews

--- src/components/ThemesOnboarding/styled.tsx
+++ src/components/ThemesOnboarding/styled.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { styled } from "@mui/material/styles";
-import { Button, Box } from "@mui/material";
+import { Button, Box, Typography } from "@mui/material";
+import { InterDisplay } from "@/mui/theme";
 
 export const StyledButton = styled(Button)(() => ({
   borderRadius: "1000px",
@@ -32,12 +33,25 @@ export const StyledPreviews = styled(Box)(({ theme }) => ({
 }));
 
 export const StyledWrap = styled(Box)(({ theme }) => ({
-  paddingBottom: 120,
+  padding: 120,
   [theme.breakpoints.down("sm")]: {
-    paddingBottom: 40,
+    padding: 40,
   },
 }));
 
 export const StyledMoreButton = styled(Box)(() => ({
   textAlign: "center",
+}));
+
+export const StyledTitle = styled(Typography)(({ theme }) => ({
+  fontSize: 48,
+  color: theme.palette.primary.main,
+  textAlign: "center",
+  fontWeight: "bold",
+  fontFamily: InterDisplay.style.fontFamily,
+  marginBottom: 64,
+  [theme.breakpoints.down("md")]: {
+    fontSize: 32,
+    marginBottom: 24,
+  },
 }));

0f6c03394875c51757ab2ae270c8877d43aeb72a
artur <brugeman.artur@gmail.com>
2025-01-30 14:58:24 +0100

    Add loginOTP

--- src/services/nostr/onboard.ts
+++ src/services/nostr/onboard.ts
@@ -1,29 +1,41 @@
 import { generatePrivateKey, getPublicKey } from "nostr-tools";
 import { addOnAuth } from "./nostr";
 
-export async function signup(username: string) {
-  const sk = generatePrivateKey();
-  const pubkey = getPublicKey(sk);
-  const event = {
-    type: 'signup',
-    method: 'local',
-    pubkey,
-    name: username,
-    localNsec: sk,
-  }
+export async function setAuth(event: any) {
   console.log("set auth", event);
 
-  // add listener before sending our event 
+  // add listener before sending our event
   // to make sure we receive the event
-  const promise = new Promise<void>(ok => {
+  const promise = new Promise<void>((ok) => {
     addOnAuth(async (type: string) => {
-      if (type === 'login') ok();
-    })
+      if (type === "login") ok();
+    });
   });
 
   // notify nostr-login
-  document.dispatchEvent(new CustomEvent('nlSetAuth', { detail: event }));
+  document.dispatchEvent(new CustomEvent("nlSetAuth", { detail: event }));
 
   // wait for onAuth from nostr-login
   await promise;
+}
+
+export async function signup(username: string) {
+  const sk = generatePrivateKey();
+  const pubkey = getPublicKey(sk);
+  return setAuth({
+    type: "signup",
+    method: "local",
+    pubkey,
+    name: username,
+    localNsec: sk,
+  });
+}
+
+export async function loginOTP(pubkey: string, otpData: any) {
+  return setAuth({
+    type: "login",
+    method: "otp",
+    pubkey,
+    otpData,
+  });
 }

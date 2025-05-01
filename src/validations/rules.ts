971f59295c629e0610d4e2ab6f4e9cd4cae24163
Алексей <aleksej@MacBook-Pro-Aleksej.local>
2025-05-01 18:22:05 +0300

    Fix build

--- src/validations/rules.ts
+++ src/validations/rules.ts
@@ -1,16 +1,16 @@
 import { nip19 } from "nostr-tools";
 import * as yup from "yup";
 
+export const validationSchemaEditNavigation = yup.object({
+  title: yup.string().required("Enter title"),
+  link: yup.string().required("Enter link"),
+});
+
 export const validationSchemaConfirmDelete = yup.object({
   confirmText: yup
     .string()
     .required("Enter confirm text")
     .oneOf(["delete"], 'The value should be "delete"'),
-});
-
-export const validationSchemaEditNavigation = yup.object({
-  title: yup.string().required("Enter title"),
-  link: yup.string().required("Enter link"),
 });
 
 const domainRegex = /^(?!:\/\/)([a-zA-Z0-9-_]{1,63}\.)*[a-zA-Z]{2,}$/;
@@ -31,7 +31,7 @@ export const validationSchemaMakePrivateSite = yup.object().shape({
       "Password is required when private",
       function (value) {
         return !(this.parent.isPrivate === true && value === undefined);
-      },
+      }
     ),
 });
 
@@ -61,7 +61,7 @@ export const validationSchemaLoginDM = yup.object({
         }
 
         return false;
-      },
+      }
     )
     .required("Required field"),
 });
